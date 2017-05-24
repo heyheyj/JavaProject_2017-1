56d07222461b3ad3413ea6cb6f25f401c112b668
Jeonseonjin <wjstjswls123@naver.com>
2017-05-24 18:14:15 +0900

    Add files via upload

--- base.java
+++ base.java
@@ -4,10 +4,10 @@ import java.awt.event.*;
 
 public class base {
 	base(){
-		Dimension dim = new Dimension(800,500);
+		Dimension dim = new Dimension(1000,1000);
 		
 		JFrame frame = new JFrame("abcd");
-		frame.setLocation(300,400);
+		frame.setLocation(10,0);
 		frame.setPreferredSize(dim);
 		
 		// 메뉴바구현
@@ -17,16 +17,43 @@ public class base {
 		JMenu filemenu = new JMenu("파일(F)");
 		filemenu.setMnemonic('F');
 			
-		JMenuItem newfile = new JMenuItem("새파일(N)");
+		JMenuItem newfile = new JMenuItem("새로 만들기(N)");
 		newfile.setMnemonic('N');
 		filemenu.add(newfile);
-			
+		
+		JMenuItem open = new JMenuItem("열기(O)");
+		open.setMnemonic('O');
+		filemenu.add(open);
+		
+		JMenuItem save = new JMenuItem("저장(S)");
+		save.setMnemonic('S');
+		filemenu.add(save);
+		
+		JMenuItem saveName = new JMenuItem("다른 이름으로 저장(A)");
+		saveName.setMnemonic('A');
+		filemenu.add(saveName);
+		
+		JMenuItem newJava = new JMenuItem(".java파일 생성(J)");
+		newJava.setMnemonic('J');
+		filemenu.add(newJava);
+		
 		JMenuItem close = new JMenuItem("닫기(C)");
 		close.setMnemonic('C');
 		filemenu.add(close);
-			
 		MenuBar.add(filemenu);
 		
+		// 툴바구현
+		JToolBar ToolBar = new JToolBar();
+		ToolBar.setLayout(new BoxLayout(ToolBar,BoxLayout.X_AXIS));
+		ToolBar.add(new JButton("새로 만들기"));
+		ToolBar.add(new JButton("열기"));
+		ToolBar.add(new JButton("저장"));
+		ToolBar.add(new JButton("다른 이름으로 저장"));
+		ToolBar.add(new JButton(".java 파일 생성"));
+		ToolBar.add(new JButton("닫기"));
+		
+		
+		//이벤트 리스너
 		/*ActionListener action = new ActionListener();{
 			public void actionPerformed(ActionEvent e){
 				Object obj = e.getSource();
@@ -43,12 +70,6 @@ public class base {
 		close.addActionListener(action);
 		*/
 		
-		
-		
-		// 툴바구현
-		JToolBar ToolBar = new JToolBar();
-		ToolBar.setLayout(new BoxLayout(ToolBar,BoxLayout.X_AXIS));
-		ToolBar.add(new JLabel("ToolBar"));
 		
 		// 속성/Editor패널구현
 		/* 속성 패널 영역 시작 */
@@ -89,13 +110,18 @@ public class base {
 		PanelEditor.setLayout(new FlowLayout(FlowLayout.RIGHT,30,40));
 		PanelEditor.add(new JLabel("Editor Pane"));
 		
+		// 속성페인과 에디터페인을 묶을 패널
+		JPanel Subpanel = new JPanel();
+		Subpanel.setLayout(new BoxLayout(Subpanel,BoxLayout.X_AXIS));
+		Subpanel.add(PanelAttribute);
+		Subpanel.add(PanelEditor);
+		
 		// 메인패널 구현(Frame위에 메뉴바,툴바,패널들 표시하기 위함)
 		JPanel Mainpanel = new JPanel();
-		Mainpanel.setLayout(new BoxLayout(Mainpanel, BoxLayout.Y_AXIS));
+		Mainpanel.setLayout(new BoxLayout(Mainpanel,BoxLayout.Y_AXIS));
 		Mainpanel.add(MenuBar);
 		Mainpanel.add(ToolBar);
-		Mainpanel.add(PanelAttribute);
-		Mainpanel.add(PanelEditor);
+		Mainpanel.add(Subpanel);
 		
 		
 		frame.add(Mainpanel);
