23903c8f4e7c671bf0da902c5517546d9154c17d
mifa031 <scifimov@naver.com>
2017-05-26 01:59:40 +0900

    Add files via upload

--- base.java
+++ base.java
@@ -2,17 +2,17 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
 
-public class base {
+public class base extends JFrame{
 	base(){
 		Dimension dim = new Dimension(1000,1000);
-		
-		JFrame frame = new JFrame("abcd");
-		frame.setLocation(10,0);
-		frame.setPreferredSize(dim);
+		setPreferredSize(dim);
+		setLocation(10,0);
+		setTitle("base");
+		setVisible(true);
+		setDefaultCloseOperation(base.EXIT_ON_CLOSE);
 		
 		// 메뉴바구현
 		JMenuBar MenuBar = new JMenuBar();
-		MenuBar.setLayout(new BoxLayout(MenuBar,BoxLayout.X_AXIS));
 			
 		JMenu filemenu = new JMenu("파일(F)");
 		filemenu.setMnemonic('F');
@@ -48,9 +48,10 @@ public class base {
 		MenuBar.add(filemenu);
 		close.addActionListener(new MenuListener());
 		
+		setJMenuBar(MenuBar);
+		
 		// 툴바구현
 		JToolBar ToolBar = new JToolBar();
-		ToolBar.setLayout(new BoxLayout(ToolBar,BoxLayout.X_AXIS));
 		ToolBar.add(new JButton("새로 만들기"));
 		ToolBar.add(new JButton("열기"));
 		ToolBar.add(new JButton("저장"));
@@ -59,8 +60,7 @@ public class base {
 		ToolBar.add(new JButton("닫기"));
 		
 		
-		// 속성/Editor패널구현
-		/* 속성 패널 영역 시작 */
+		// 속성 패널구현
 		JPanel PanelAttribute = new JPanel();
 		PanelAttribute.setLayout(new GridLayout(7,2,0,5));
 		
@@ -92,31 +92,21 @@ public class base {
 		PanelAttribute.add(new JLabel("변수명 : "));
 		JTextField compName = new JTextField("");
 		PanelAttribute.add(compName);
-		/* 속성 패널 영역 끝 */
 		
+		// editor 패널 구현
 		JPanel PanelEditor = new JPanel();
-		PanelEditor.setLayout(new FlowLayout(FlowLayout.RIGHT,30,40));
 		PanelEditor.add(new JLabel("Editor Pane"));
 		PanelEditor.addMouseListener(new EditorListener());
 		
-		// 속성페인과 에디터페인을 묶을 패널
-		JPanel Subpanel = new JPanel();
-		Subpanel.setLayout(new BoxLayout(Subpanel,BoxLayout.X_AXIS));
-		Subpanel.add(PanelAttribute);
-		Subpanel.add(PanelEditor);
-		
-		// 메인패널 구현(Frame위에 메뉴바,툴바,패널들 표시하기 위함)
-		JPanel Mainpanel = new JPanel();
-		Mainpanel.setLayout(new BoxLayout(Mainpanel,BoxLayout.Y_AXIS));
-		Mainpanel.add(MenuBar);
-		Mainpanel.add(ToolBar);
-		Mainpanel.add(Subpanel);
-		
-		
-		frame.add(Mainpanel);
-		frame.pack();
-		frame.setVisible(true);
+		//frame 전체를 BorderLayout으로 설정
+		Container contentPane = getContentPane();
+		contentPane.setLayout(new BorderLayout());
+		contentPane.add(ToolBar, BorderLayout.NORTH);
+		contentPane.add(PanelAttribute,BorderLayout.WEST);
+		contentPane.add(PanelEditor,BorderLayout.CENTER);
+		pack();
 	}
+	
 	
 	private class MenuListener implements ActionListener{
 		@Override
@@ -152,7 +142,6 @@ public class base {
 
 		@Override
 		public void mouseClicked(MouseEvent e) {
-
 		}
 
 		@Override
@@ -177,6 +166,6 @@ public class base {
 		
 	}
 	public static void main(String[] args){
-		base Base = new base();		
+		new base();	
 	}
 }
